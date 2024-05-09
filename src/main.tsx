7e24dc7d7480111a282adf6ee8973abc7c85d5d7
Joan C <joan0492@gmail.com>
2024-05-08 22:33:21 -0500

    change hashrouter for upload gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "@/App.tsx";
 import "@/assets/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
